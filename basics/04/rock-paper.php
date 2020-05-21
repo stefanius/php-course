2e0b80b1dd9994d9e5508039a90d4e1379dc80b2
stefanius <sgrootveld@testmonitor.com>
2020-05-21 20:43:01 +0200

    --- Auto-Git Commit ---

--- basics/04/rock-paper.php
+++ basics/04/rock-paper.php
@@ -1,7 +1,14 @@
 <?php
 
-$speler1 = beurt();
+$speler1 = $argv[1];
+// $speler1 = beurt();
 $speler2 = beurt();
+
+print(uitslag($speler1, $speler2) .  "\r\n");
+
+print("-------------------------" .  "\r\n");
+print("speler 1: " . $speler1 . "\r\n");
+print("speler 2: " . $speler2 .  "\r\n");
 
 function beurt() {
     $worp = rand(0,2);
@@ -15,4 +22,18 @@ function beurt() {
     return $opties[$worp];
 }
 
-print($speler1);
+function uitslag($speler1, $speler2) {
+    if ($speler1 === $speler2) {
+        return "GELIJKSPEL";
+    }
+
+    if (
+        ($speler1 === "STEEN" && $speler2 === "PAPIER") ||
+        ($speler1 === "PAPIER" && $speler2 === "SCHAAR") ||
+        ($speler1 === "SCHAAR" && $speler2 === "STEEN")
+        ) {
+        return "SPELER 2 WINT";
+    }
+
+    return "SPELER 1 WINT";
+}
